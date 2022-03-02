c361f7cb6963df2428b446b1c32efbc9d02c50dd
Rechner Fox <659028+rechner@users.noreply.github.com>
2022-03-01 20:45:30 -0800

    Bump version

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name='aamva',
-    version='0.2.0',
+    version='0.2.1',
     packages=['aamva'],
     url='https://github.com/rechner/py-aamva',
     license='GPLv2',
